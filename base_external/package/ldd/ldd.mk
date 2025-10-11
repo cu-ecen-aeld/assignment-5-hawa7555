714449f99fc4786d442ece257da767b863e9cc12
Harshal Wadhwa <hawa7555@colorado.edu>
2025-10-11 13:04:42 -0600

    Adding modules to build  in buildroot

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -5,13 +5,21 @@
 #
 ##############################################################
 
-LDD_VERSION = 432c11f06e47449bd14e7d0c447a80fc34b2ae62
-LDD_SITE = https://github.com/cu-ecen-aeld/assignments3-and-later-hawa7555.git
+LDD_VERSION = 'd7d4a5b97be6c71c0d0977191cd08bfb689ff91b'
+LDD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-hawa7555.git'
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
 
 LDD_MODULE_SUBDIRS = misc-modules
 LDD_MODULE_SUBDIRS += scull
 
+define LDD_INSTALL_TARGET_CMDS
+    $(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin/
+    $(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin/
+    $(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin/
+    $(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin/
+endef
+
 $(eval $(kernel-module))
 $(eval $(generic-package))
+
